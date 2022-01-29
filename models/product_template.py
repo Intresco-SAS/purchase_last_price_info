bae53417ee0e1fe93eb3e9f1614d536cb7da39d8
Nelson V <nelson.avelez@hotmail.com>
2022-01-29 12:23:01 -0500

    Ultimo_Precio_Con_Impuesto

--- models/product_template.py
+++ models/product_template.py
@@ -44,6 +44,10 @@ class ProductTemplate(models.Model):
         digits=0,
     )
 
+    standard_price_with_taxes = fields.Float(
+        'Costo con Impuesto', compute='_compute_standard_price_with_taxes',
+        digits='Product Price', groups="base.group_user")
+
     @api.depends("last_purchase_line_ids")
     def _compute_last_purchase_line_id(self):
         for item in self:
@@ -56,3 +60,17 @@ class ProductTemplate(models.Model):
             item.last_purchase_date = item.last_purchase_line_id.date_order
             item.last_purchase_supplier_id = item.last_purchase_line_id.partner_id
             item.last_purchase_currency_id = item.last_purchase_line_id.currency_id
+    
+    @api.onchange('last_purchase_price', 'supplier_taxes_id')
+    def _onchange_product_cost_taxes(self):
+        for product in self:
+            product._compute_standard_price_with_taxes()
+
+    def _compute_standard_price_with_taxes(self):
+        for product in self:
+            product.standard_price_with_taxes = 0.0
+            if product.supplier_taxes_id:
+                taxes = product.supplier_taxes_id[0].compute_all(product.last_purchase_price, product.cost_currency_id, 1, product=product, partner=False)
+                # price_subtotal = taxes['total_excluded']
+                price_subtotal_incl = taxes['total_included']
+                product.standard_price_with_taxes = price_subtotal_incl
